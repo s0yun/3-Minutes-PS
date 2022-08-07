557337cba14e77d53f4700ae87ca2b52cf0d2d1e
Christopher Pryazhentsev <christopherpryazhentsev@gmail.com>
2022-08-06 21:21:33 -0400

    replaced write-output with write-host

--- 3minutes.ps1
+++ 3minutes.ps1
@@ -1,4 +1,4 @@
-Write-Output @'
+Write-Host @'
 #######################################
 #                                     #
 #        3 Minutes - Bring Focus      #
@@ -6,8 +6,7 @@ Write-Output @'
 #######################################
 '@
 
-
-Write-Output "We're going to take a couple of nice deep breaths now `nReady?"
+Write-Host "We're going to take a couple of nice deep breaths now `nReady?"
 Start-Sleep 1
 [int]$breaths = 0
 do {
@@ -15,16 +14,16 @@ $breaths = $breaths +1
 [int]$second = 0
     do {
         $second = $second +1
-        Write-Output "Inhale for $second"
+        Write-Host "Inhale for $second"
         Start-Sleep 1
         } until (
         $second -eq 4
     )
-    Write-Output 'Great! You are doing great!'
+    Write-Host 'Great! You are doing great!'
     [int]$second = 0
     do {
         $second = $second +1
-        Write-Output "Hold it in for $second"
+        Write-Host "Hold it in for $second"
         Start-Sleep 1
         } until (
         $second -eq 3
@@ -32,19 +31,19 @@ $breaths = $breaths +1
     [int]$second = 0
     do {
         $second = $second +1
-        Write-Output "Exhale for $second"
+        Write-Host "Exhale for $second"
         Start-Sleep 1
         } until (
         $second -eq 4
     )
     if ($breaths -ne 2) {
-        Write-Output 'And again,'
+        Write-Host 'And again,'
     }
 } until (
     $breaths -eq 2
 )
 
-Write-Output "Now it is time for you to close your eyes, we have a timer `nit is set for three minutes, after this I will tell you that your time is up"
+Write-Host "Now it is time for you to close your eyes, we have a timer `nit is set for three minutes, after this I will tell you that your time is up"
 
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Are you ready to take a few moments: Yes'
 $no = New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'Are you ready to take a few moments: No'
@@ -54,14 +53,14 @@ $message = 'Are you ready to take a few moments?'
 $result = $host.ui.PromptForChoice($title, $message, $options, 0)
 
 switch ($result) {
-    0 {Write-Output "Great, we will get started. `nClose your eyes, the three minutes starts now"
+    0 {Write-Host "Great, we will get started. `nClose your eyes, the three minutes starts now"
         Start-Sleep 300}
-    1 {Write-Output "Okay, perhaps next time."}
+    1 {Write-Host "Okay, perhaps next time."}
     }
 
 $voice = New-Object -ComObject Sapi.spvoice
 $voice.rate = 1
 $voice.speak("Hey, your time is up!")
 
-Write-Output 'Thanks for taking a few minutes with me'
+Write-Host 'Thanks for taking a few minutes with me'
 Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
